8526e482eb29d78127353baf6f757fb933715530
au706600 <202106601@post.au.dk>
2024-09-26 17:09:39 +0200

    The player can move up and down and boundaries are added.

--- Pong-game/Form1.Designer.cs
+++ Pong-game/Form1.Designer.cs
@@ -63,6 +63,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(29, 66);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // pictureBox2
             // 
@@ -184,6 +185,8 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
--- Pong-game/Form1.cs
+++ Pong-game/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,12 +14,14 @@ using System.Windows.Forms;
 // 1) The ball should be bounced, whenever, it hits the "players". 
 // 2) When it exceeds the player, the counter/point of the player should be incremented.
 // 3) It should be played against computer and eventually against a player. 
-// 4) To start, the player should be able to move up and down.
+// 4) To start, the player should be able to move up and down. Check
+// 5) Add boundary so that player shouldn't be able to move off the boundary. Check
 
 namespace Pong_game
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
@@ -26,9 +29,46 @@ namespace Pong_game
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //this.KeyUp += new KeyEventHandler(Form1_KeyUp); 
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+        }
+
+        // The right side player
+       
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
             
         }
 
-      
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int y = pictureBox1.Location.Y;
+            int x = pictureBox1.Location.X;
+            int Y_Size = Form1.ActiveForm.Height;
+            switch (e.KeyCode)
+            {
+                case Keys.Down:
+                    if(y + pictureBox1.Height <= this.ClientSize.Height)
+                    {
+                        y += 5;
+                    }
+                    break;
+
+                case Keys.Up:
+                    if(y >= 0)
+                    {
+                        y -= 5;
+                    }
+                    break;
+            }
+   
+            pictureBox1.Location = new Point(x, y);
+            this.Invalidate();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
     }
 }
